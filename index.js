17553f69c8eb1edbabf2b4b6e234ea97fdf950a5
Durga Prasad <84697619+Durgaprasad-2002@users.noreply.github.com>
2023-08-23 15:05:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const { MongoClient, ObjectId } = require("mongodb");
 const { mongo } = require("mongoose");
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
